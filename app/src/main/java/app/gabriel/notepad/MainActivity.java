406cf0468736b9d7abb5f53000b784b753bbd5b6
Gabriel Lima <mugiwara.gl@gmail.com>
2018-03-26 14:05:28 -0300

    view, e versões compativeis com os metodos. versão 0.1.3

--- app/src/main/java/app/gabriel/notepad/MainActivity.java
+++ app/src/main/java/app/gabriel/notepad/MainActivity.java
@@ -1,40 +1,103 @@
 package app.gabriel.notepad;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth fAuth;
+
+    private RecyclerView mNotesList;
+    private GridLayoutManager gridLayoutManager;
+    private DatabaseReference fNotesDatabase;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mNotesList = (RecyclerView) findViewById( R.id.main_notes_layout );
+
+        gridLayoutManager = new GridLayoutManager( this,1, GridLayoutManager.VERTICAL,false );
+
+        mNotesList.setHasFixedSize( true );
+        mNotesList.setLayoutManager( gridLayoutManager );
+
+
         fAuth = FirebaseAuth.getInstance();
-
+        if(fAuth.getCurrentUser() != null){
+            fNotesDatabase = FirebaseDatabase.getInstance().getReference().child( "Notas" ).child( fAuth.getCurrentUser().getUid() );
+        }
         updateUI();
+
     }
 
-    private void updateUI(){
 
-            if(fAuth.getCurrentUser() != null){
-                   Log.i("MainActivity", "fAuth != null");
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerAdapter<NoteModel, NoteViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<NoteModel, NoteViewHolder>(
+                NoteModel.class,
+                        R.layout.single_note_layout,
+                NoteViewHolder.class,
+                        fNotesDatabase
+
+
+                ) {
+            @Override
+            protected void populateViewHolder(final NoteViewHolder viewHolder, NoteModel model, int position) {
+                String NoteId = getRef( position ).getKey();
+
+                fNotesDatabase.child( NoteId ).addValueEventListener( new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        String title = dataSnapshot.child( "title" ).getValue().toString();
+                        String timestamp = dataSnapshot.child( "timestamp" ).getValue().toString();
+
+                        viewHolder.setNoteTitle( title );
+                        viewHolder.setNoteTime( timestamp );
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                } );
+
             }
-            else{
-                Intent StartIntent = new Intent(MainActivity.this,StartActivity.class);
-                startActivity(StartIntent);
-                finish();
-                Log.i("MainActivity", "fAuth == null");
-            }
+        };
+
+         mNotesList.setAdapter( firebaseRecyclerAdapter );
     }
+
+    private void updateUI() {
+
+        if (fAuth.getCurrentUser() != null) {
+            Log.i( "MainActivity", "fAuth != null" );
+        } else {
+            Intent StartIntent = new Intent( MainActivity.this, StartActivity.class );
+            startActivity( StartIntent );
+            finish();
+            Log.i( "MainActivity", "fAuth == null" );
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -51,7 +114,7 @@ public class MainActivity extends AppCompatActivity {
 
         switch (item.getItemId()){
             case R.id.main_new_note_btn:
-                Intent intent = new Intent(MainActivity.this,NewNoteActivity.class);
+                Intent intent = new Intent(MainActivity.this, NewNoteActivity.class);
                 startActivity(intent);
                 break;
         }
---
+++ app/src/main/java/app/gabriel/notepad/NoteViewHolder.java
@@ -0,0 +1,35 @@
+package app.gabriel.notepad;
+
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by Gabriel on 25/03/2018.
+ */
+
+public class NoteViewHolder extends ViewHolder {
+
+    View view;
+
+    TextView textTitle, textTime;
+
+    public NoteViewHolder(View itemView) {
+        super(itemView);
+
+        view = itemView;
+
+        textTitle = (TextView) view.findViewById(R.id.note_title);
+        textTime = (TextView)  view.findViewById(R.id.note_time);
+    }
+
+    public void setNoteTitle(String title){
+        textTitle.setText(title);
+
+    }
+
+    public void setNoteTime(String time){
+
+        textTime.setText(time);
+    }
+}
